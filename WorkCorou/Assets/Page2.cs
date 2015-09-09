e02cf7b618309d9c809d952994c1af6944033fa1
yuichan005 <hamtaro005@hotmail.com>
2015-09-09 23:42:33 +0700

    coroutine1

---
+++ WorkCorou/Assets/Page2.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Page2 : MonoBehaviour {
+
+		
+		public Texture mybg;
+		
+		
+		void OnGUI () {
+			
+			GUI.DrawTexture(new Rect(0f, 0f, Screen.width, Screen.height), mybg);
+			
+			
+		}
+		
+	
+
+}
---
+++ WorkCorou/Assets/TestCorou.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestCorou : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine (waitToShow (5f));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	IEnumerator waitToShow (float seconds)
+	{
+		Debug.Log("Wait 5 milliseconds for Next Page");
+		yield return new WaitForSeconds (seconds);
+		Debug.Log ("Page2");
+		Application.LoadLevel("NextPage");
+	}
+}
